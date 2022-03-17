32d9a8aaa55a0fafd9a0fe330c77925465e635a3
Luis Rangel <luisrangelc@gmail.com>
2022-03-17 08:19:21 -0600

    feat: const and let

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,21 @@
+console.log('Hello World!!!');
+
+// Variables y Constantes
+
+const nombre = 'Luis';
+const apellido = 'Rangel';
+const xx = 'hello';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+console.log(xx);
+
+if (true) {
+  let valorDado = 6
+  const xx = 'bye';
+  console.log(valorDado);
+  console.log(xx);
+}
+// let
