8ebf0fabbd48d1aea6a66dea0947d4df7e17feb0
Luis Rangel <luisrangelc@gmail.com>
2022-03-23 07:36:12 -0600

    feat: onClick

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -2,10 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
+  // handleAdd
+  const handleAdd = (e) => {
+    console.log(e);
+    value++;
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+
+      {/* <button
+        onClick={(e) => {
+          handleAdd(e);
+        }}
+      >
+        +1
+      </button> */}
+
+      {/* <button
+        onClick={(e) => {
+          console.log(e);
+        }}
+      >
+        +1
+      </button> */}
+
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
