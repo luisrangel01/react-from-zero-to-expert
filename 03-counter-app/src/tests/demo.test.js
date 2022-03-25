311ba31829f2314ffe74bc744df543173e197b31
Luis Rangel <luisrangelc@gmail.com>
2022-03-24 21:23:07 -0600

    feat: Jest - Expect - toBe

--- 03-counter-app/src/tests/demo.test.js
+++ 03-counter-app/src/tests/demo.test.js
@@ -1,6 +1,12 @@
-test('debe de ser true', () => {
-  const isActive = true;
-  if (isActive) {
-    throw new Error('No esta activo');
-  }
+describe('Pruebas en el archivo demo.test.js', () => {
+  test('debe de ser iguales los strings', () => {
+    // 1. inicializacion
+    const mensaje = 'Hello World!';
+
+    // 2. estimulo
+    const mensaje2 = `Hello World!`;
+
+    // 3. Observar el comportamiento
+    expect(mensaje).toBe(mensaje2);
+  });
 });
