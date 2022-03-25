842b093315e43f8d4ec27ee1619f62c28fbb1348
Luis Rangel <luisrangelc@gmail.com>
2022-03-24 20:32:46 -0600

    feat: add spread

--- flat.js
+++ flat.js
@@ -2,13 +2,14 @@ console.clear();
 
 const complexArray = [300, 1, [8, 9, 12], 2, ['a', 'b', 'd', [66, 52]], 3, 4, 5, [442]];
 
-console.log('originArray', complexArray);
+console.log('originArray', complexArray, `length: ${complexArray.length}`);
 
 const flatArray = (originArray) => {
   const newArray = [];
   originArray.forEach((element) => {
     if (Array.isArray(element)) {
-      newArray.push.apply(newArray, flatArray(element));
+      newArray.push(...flatArray(element));
+      // newArray.push.apply(newArray, flatArray(element));
     } else {
       newArray.push(element);
     }
@@ -16,4 +17,5 @@ const flatArray = (originArray) => {
   return newArray;
 };
 
-console.log('Array Flat', flatArray(complexArray));
+const flat = flatArray(complexArray);
+console.log('Array Flat', flat, `length: ${flat.length}`);
