49e8275cad925272712aada789820ca75b1975fd
Luis Rangel <luisrangelc@gmail.com>
2022-03-28 08:24:55 -0600

    feat: test component

---
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import PrimeraApp from '../PrimeraApp';
+
+describe('Pruebas en <PrimeraApp />', () => {
+  test('debe de mostrar el mensaje "Hola, soy Goku"', () => {
+    const saludo = 'Hola, soy Goku';
+
+    const { getByText } = render(<PrimeraApp saludo={saludo} />);
+
+    expect(getByText(saludo)).toBeInTheDocument();
+  });
+});
