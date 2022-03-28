fbf89156e30c20a5c22cff60357eddf410f2e32c
Luis Rangel <luisrangelc@gmail.com>
2022-03-28 08:04:24 -0600

    feat: test async await

--- 03-counter-app/src/base/11-async-await.js
+++ 03-counter-app/src/base/11-async-await.js
@@ -1,32 +1,21 @@
 
+export const getImagen = async () => {
+  try {
+    const apiKey = 'dUaEpCHLvbS1VDigGQT0Zp6ge5yepPT5';
+    const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+    const { data } = await resp.json();
 
-// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
-// getImagenPromesa().then( console.log );
+    const { url } = data.images.original;
 
-const getImagen = async() => {
+    // const img = document.createElement('img');
+    // img.src = url;
+    // document.body.append(img);
 
-    try {
-
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
-
-        const { url } = data.images.original;
-
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
-
-    } catch (error) {
-        // manejo del error
-        console.error(error)
-    }
-    
-    
-    
-}
-
- getImagen();
-
-
-
+    return url;
+  } catch (err) {
+    // manejo del error
+    console.error(err);
+    // return err;
+    return 'No existe';
+  }
+};
