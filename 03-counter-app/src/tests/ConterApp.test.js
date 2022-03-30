ebee33220f8d01f84e990f61adf35c55653e4d26
Luis Rangel <luisrangelc@gmail.com>
2022-03-30 07:07:32 -0600

    feat: beforeEach

--- 03-counter-app/src/tests/ConterApp.test.js
+++ 03-counter-app/src/tests/ConterApp.test.js
@@ -4,9 +4,13 @@ import { shallow } from 'enzyme';
 import CounterApp from '../CounterApp';
 
 describe('Pruebas en <CounterApp />', () => {
-  test('debe de mostrar <CounterApp />', () => {
-    const wrapper = shallow(<CounterApp />);
+  let wrapper = shallow(<CounterApp />);
 
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);
+  });
+
+  test('debe de mostrar <CounterApp />', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
@@ -14,9 +18,33 @@ describe('Pruebas en <CounterApp />', () => {
     const value = 100;
     const wrapper = shallow(<CounterApp value={value} />);
 
-    const textoTitulo = wrapper.find('h2').text().trim();
-    console.log({ textoTitulo });
+    const textoCounter = wrapper.find('h2').text().trim();
+    console.log({ textoCounter });
 
-    expect(textoTitulo).toBe(value.toString());
+    expect(textoCounter).toBe(value.toString());
+  });
+
+  test('debe de incrementar con el boton +1', () => {
+    // const bt1 = wrapper.find('button').at(0);
+    // console.log(`bt1:`);
+    // console.log(bt1.html);
+
+    wrapper.find('button').at(0).simulate('click');
+    const value = 11;
+
+    const textoCounter = wrapper.find('h2').text().trim();
+    console.log({ textoCounter });
+
+    expect(textoCounter).toBe(value.toString());
+  });
+
+  test('debe de decrementar con el boton -1', () => {
+    wrapper.find('button').at(2).simulate('click');
+    const value = 9;
+
+    const textoCounter = wrapper.find('h2').text().trim();
+    console.log({ textoCounter });
+
+    expect(textoCounter).toBe(value.toString());
   });
 });
