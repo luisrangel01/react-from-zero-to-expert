22c0d2ed5716926b9151478c74ee34034c8a36ae
Luis Rangel <luisrangelc@gmail.com>
2022-03-31 22:02:37 -0600

    feat: GifExpertApp component

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const GifExpertApp = () => {
+  // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+  const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+  const handleAdd = () => {
+    setCategories([...categories, `xxx-${categories.length + 1}`]);
+  };
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <hr />
+      <button onClick={handleAdd}>Agregar</button>
+      <ol>
+        {categories.map((category, index) => {
+          return <li key={index}>{category}</li>;
+        })}
+      </ol>
+    </>
+  );
+};
+
+export default GifExpertApp;
