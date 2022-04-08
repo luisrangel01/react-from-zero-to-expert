c6950303995df1e5cdecd080e2e76fd8fb40d3d4
Luis Rangel <luisrangelc@gmail.com>
2022-04-07 21:37:09 -0600

    feat: animations

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -9,7 +9,7 @@ export const GifGrid = ({ category, index }) => {
   return (
     <>
       <h3 key={`h3${index}`}>{category}</h3>
-      {loading && <p>Loading...</p>}
+      {loading && <p className='animate__animated animate__flash'>Loading...</p>}
       <div className='card-grid'>
         {data.map((img) => (
           <GifGridItem key={img.id} img={img}></GifGridItem>
--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export const GifGridItem = ({ img }) => {
   return (
-    <div className='card'>
+    <div className='card animate__animated animate__fadeIn animate__delay-2s'>
       <img src={img.url} alt={img.title}></img>
       <p>{img.title}</p>
     </div>
   );
 };
+
+{/* <div class="animate__animated animate__bounce animate__delay-2s">Example</div> */}
