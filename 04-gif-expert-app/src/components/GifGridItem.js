81d43fe2a090047faac05aab2586143e2ca0b416
Luis Rangel Castro <luisrangelc@gmail.com>
2022-04-15 12:47:36 -0600

    feat: add props

--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const GifGridItem = ({ img }) => {
+export const GifGridItem = ({ img, title, url }) => {
   return (
     <div className='card animate__animated animate__fadeIn animate__delay-2s'>
-      <img src={img?.url} alt={img?.title}></img>
-      <p>{img?.title}</p>
+      <img src={url ?? img.url} alt={title ?? img.title}></img>
+      <p>{title ?? img.title}</p>
     </div>
   );
+};
+
+GifGridItem.propTypes = {
+  // title: PropTypes.string.isRequired,
+  // url: PropTypes.string.isRequired,
+  img: PropTypes.object.isRequired,
+};
+
+GifGridItem.defaultProps = {
+  img: { url: '', title: '' },
 };
--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -4,8 +4,15 @@ import { shallow } from 'enzyme';
 import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => {
+  const title = 'Un titulo';
+  const url = 'https://localhost/algo.jpg';
+  const img = {
+    title,
+    url,
+  };
+
   test('debe de mostrar el componente correctamente', () => {
-    const wrapper = shallow(<GifGridItem />);
+    const wrapper = shallow(<GifGridItem img={img} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
