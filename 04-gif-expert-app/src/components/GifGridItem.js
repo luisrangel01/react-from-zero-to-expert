dcc848929821d143324e2fa3b197760313585dbf
Luis Rangel Castro <luisrangelc@gmail.com>
2022-04-15 12:29:04 -0600

    feat: add test to GifGridItem

--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -3,8 +3,8 @@ import React from 'react';
 export const GifGridItem = ({ img }) => {
   return (
     <div className='card animate__animated animate__fadeIn animate__delay-2s'>
-      <img src={img.url} alt={img.title}></img>
-      <p>{img.title}</p>
+      <img src={img?.url} alt={img?.title}></img>
+      <p>{img?.title}</p>
     </div>
   );
 };
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # react-from-zero-to-expert
+
+# test component
+
+shallow
+wrapper
+wrapper .toMatchSnapshot()
+
+al correr este comando 'expect(wrapper).toMatchSnapshot();' genera un snapshots si no existe y lo compara
